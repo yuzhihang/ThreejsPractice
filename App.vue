d2993ef51c6154342afaa1dedabb83f760be245c
yuzhihang <rossonero@sjtu.org>
2019-04-26 18:53:28 +0800

    use import syntax

--- App.vue
+++ App.vue
@@ -1,14 +1,21 @@
 <template>
+
     <div id="app">
-        <img alt="Vue logo" src="./assets/logo.png">
+        <div id="stats-output">
+
+        </div>
+        <!--<img alt="Vue logo" src="./assets/logo.png">-->
         <!--<HelloWorld msg="Welcome to Your Vue.js App"/>-->
     </div>
 </template>
 
 <script>
     // import HelloWorld from './components/HelloWorld.vue'
-    import * as THREE from 'three'
-import State from ''
+    // import * as THREE from 'three'
+    import THREE from './learning-threejs-master/libs/three'
+    import Stats from './learning-threejs-master/libs/stats'
+    import dat from './learning-threejs-master/libs/dat.gui'
+    // import State from 'sta'
 
     export default {
         // elm:'#app',
@@ -27,77 +34,126 @@ import State from ''
         },
         methods: {
             init() {
-                let scene = new THREE.Scene()
-                let camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000)
-                let renderer = new THREE.WebGLRenderer()
-                renderer.setClearColor(0xEEEEEE, 1.0)
-                renderer.setSize(window.innerWidth, window.innerHeight)
-                renderer.shadowMap.enabled = true
-                renderer.shadowMap.type = THREE.PCFSoftShadowMap;//防止阴影马赛克
 
-                let axes = new THREE.AxesHelper(20)
-                scene.add(axes)
+                window.addEventListener('resize', this.onResize, false)
 
-                let planeGeometry = new THREE.PlaneGeometry(60, 20, 1, 1)
-                let planeMaterial = new THREE.MeshLambertMaterial({color: 0xcccccc});
+                this.scene = new THREE.Scene();
 
-                let plane = new THREE.Mesh(planeGeometry, planeMaterial)
+                // create a this.camera, which defines where we're looking at.
+                this.camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-                plane.rotation.x = -Math.PI >> 2
-                plane.position.x = 15
-                plane.position.y = 0
-                plane.position.z = 0
+                // create a render and set the size
+                this.renderer = new THREE.WebGLRenderer();
+
+                this.renderer.setClearColor(new THREE.Color(0xEEEEEE, 1.0));
+                this.renderer.setSize(window.innerWidth, window.innerHeight);
+                this.renderer.shadowMapEnabled = true;
+
+                // create the ground plane
+                var planeGeometry = new THREE.PlaneGeometry(60, 20, 1, 1);
+                var planeMaterial = new THREE.MeshLambertMaterial({color: 0xffffff});
+                var plane = new THREE.Mesh(planeGeometry, planeMaterial);
                 plane.receiveShadow = true;
 
-                let cubeGeometry = new THREE.BoxGeometry(4, 4, 4)
-                let cubeMaterial = new THREE.MeshLambertMaterial({color: 0xff0000})
-                let cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+                // rotate and position the plane
+                plane.rotation.x = -0.5 * Math.PI;
+                plane.position.x = 15;
+                plane.position.y = 0;
+                plane.position.z = 0;
 
-                cube.position.x = -4
-                cube.position.y = 3
-                cube.position.z = 0
+                // add the plane to the this.scene
+                this.scene.add(plane);
 
+                var axes = new THREE.AxisHelper(20);
+                this.scene.add(axes);
+
+                // create a cube
+                var cubeGeometry = new THREE.BoxGeometry(4, 4, 4);
+                var cubeMaterial = new THREE.MeshLambertMaterial({color: 0xff0000});
+                var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
                 cube.castShadow = true;
 
-                scene.add(cube)
+                // position the cube
+                cube.position.x = -4;
+                cube.position.y = 3;
+                cube.position.z = 0;
 
+                // add the cube to the this.scene
+                this.scene.add(cube);
 
-                let sphereGeometry = new THREE.SphereGeometry(4, 20, 20)
-                let sphereMaterial = new THREE.MeshLambertMaterial({
-                    color: 0x7777ff,
-                    // wireframe: true
-                });
-                let sphere = new THREE.Mesh(sphereGeometry, sphereMaterial)
+                var sphereGeometry = new THREE.SphereGeometry(4, 20, 20);
+                var sphereMaterial = new THREE.MeshLambertMaterial({color: 0x7777ff});
+                var sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
 
-                sphere.position.x = 20
-                sphere.position.y = 4
-                sphere.position.z = 2
-                scene.add(sphere)
-                sphere.castShadow = true
+                // position the sphere
+                sphere.position.x = 20;
+                sphere.position.y = 0;
+                sphere.position.z = 2;
+                sphere.castShadow = true;
 
+                // add the sphere to the this.scene
+                this.scene.add(sphere);
 
-                camera.position.x = -30
-                camera.position.y = 40
-                camera.position.z = 30
-                camera.lookAt(scene.position)
-                // Object.assign(plane, {position: {x: 15, y: 0, z: 0}})
-                scene.add(plane)
+                // position and point the this.camera to the center of the this.scene
+                this.camera.position.x = -30;
+                this.camera.position.y = 40;
+                this.camera.position.z = 30;
+                this.camera.lookAt(this.scene.position);
 
-                let spotLight = new THREE.SpotLight(0xffffff)
-                spotLight.position.set(-40, 60, -10)
+                // add subtle ambient lighting
+                var ambientLight = new THREE.AmbientLight(0x0c0c0c);
+                this.scene.add(ambientLight);
+
+                var spotLight = new THREE.SpotLight(0xffffff);
+                spotLight.position.set(-40, 60, -10);
                 spotLight.castShadow = true;
-                scene.add(spotLight)
+                this.scene.add(spotLight);
 
-                function renderScene(){
-                    requestAnimationFrame(renderScene);
-                    renderer.render(scene, camera);
+                let step = 0
+
+                let renderAnimation = () => {
+                    stats.update()
+
+                    cube.rotation.x += controls.rotationSpeed
+                    cube.rotation.y += controls.rotationSpeed
+                    cube.rotation.z += controls.rotationSpeed
+
+                    step += controls.bouncingSpeed
+                    sphere.position.x = 20 + (10 * (Math.cos(step)))
+                    sphere.position.y = 2 + (10 * (Math.sin(step)))
+
+                    requestAnimationFrame(renderAnimation)
+                    this.renderer.render(this.scene, this.camera)
                 }
 
-                document.getElementById('app').appendChild(renderer.domElement)
-                renderScene();
+                let controls = {rotationSpeed: 0.02, bouncingSpeed: 0.05}
+                let gui = new dat.GUI();
+                gui.add(controls, 'rotationSpeed', 0, 0.5)
+                gui.add(controls, 'bouncingSpeed', 0, 0.5)
+
+                // add the output of the this.renderer to the html element
+                document.getElementById("app").appendChild(this.renderer.domElement)
+                let stats = this.initState()
+
+                // call the render function
+                renderAnimation();
+            },
+            initState() {
+                let stats = new Stats()
+                stats.setMode(0)
+                stats.domElement.style.position = 'absolute'
+                stats.domElement.style.left = '0'
+                stats.domElement.style.right = '0'
+                document.getElementById('stats-output')
+                    .appendChild(stats.domElement)
+                return stats
+            },
+            onResize() {
+                this.camera.aspect = window.innerWidth / window.innerHeight
+                this.camera.updateProjectionMatrix()
+                this.renderer.setSize(window.innerWidth, window.innerHeight)
             }
 
-            ,
         }
     }
 </script>
@@ -111,5 +167,14 @@ import State from ''
         text-align: center;
         color: #2c3e50;
         margin-top: 60px;
+        width: 100%;
+        /*height:100%;*/
+    }
+
+    #stats-output {
+        position:absolute;
+        top:0;
+        width:20%;
+        height:50px;
     }
 </style>
